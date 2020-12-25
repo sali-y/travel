8905c06cc055ab2a57107a32b0455c5a14f24a65
sali-y <76592615+sali-y@users.noreply.github.com>
2020-12-25 21:53:52 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { BrowserRouter, Redirect } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import TravelCard from './components/TravelCard/TravelCard';
-import TravelDetails from './tripDetails/tripDetails';
-import UserList from './users/userList';
+import TravelDetails from './components/tripDetails/tripDetails';
+import UserList from './components/users/userList';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <TravelCard trip={trip}/> */}
       <Route path="/" exact component={TravelCard} />
       <Route path="/details" exact component={TravelDetails} />
       <Route path="/users" exact component={UserList} />
